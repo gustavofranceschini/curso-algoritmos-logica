d66a134597f84b81c9e87de3e9f5e33644df2ca0
Gustavo Franceschini <gustavo.araujo18@fatec.sp.gov>
2022-06-16 09:23:44 -0300

    Linguagem C

---
+++ Linguagem C/Sequencial/SOMA/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int soma,a,b = 0;
+
+    printf("Digite o primeiro numero: ");
+    scanf("%d",&a);
+    printf("Digite o segundo numero: ");
+    scanf("%d",&b);
+
+    soma = a + b ;
+
+    printf("SOMA = %d",soma);
+
+    return 0;
+}
